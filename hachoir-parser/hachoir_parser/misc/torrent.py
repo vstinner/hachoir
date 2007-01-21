70527066f13a47cce7d6569780065779f7b718e4
haypo <devnull@localhost>
2007-01-21 15:58:21 +0000

    Torrent:  * Remove unused imports  * Set arbitrary limits to string length and integer value  * Split Entry.createFields() into subfunctions  * Create class DictionaryItem for dictionary items  * Add magic to TorrentFile for hachoir-subfile  * Replace info() with self.info()

--- hachoir-parser/hachoir_parser/misc/torrent.py
+++ hachoir-parser/hachoir_parser/misc/torrent.py
@@ -9,106 +9,139 @@ Author: Christophe Gisquet <christophe.gisquet@free.fr>
 
 from hachoir_parser import Parser
 from hachoir_core.field import (FieldSet, ParserError,
-    Bit, Bits, Enum,
-    UInt8, UInt16, UInt32, UInt64,
-    String,
-    RawBytes)
+    String, RawBytes)
 from hachoir_core.text_handler import (humanFilesize,
-    hexadecimal,
     humanDatetime, doTimestampUNIX)
 from hachoir_core.endian import LITTLE_ENDIAN
-from hachoir_parser.archive.rar import MSDOSFileAttr
-from hachoir_core.error import info
+from hachoir_core.tools import makePrintable
+
+# Maximum number of bytes for string length
+MAX_STRING_LENGTH = 6   # length in 0..999999
+
+# Maximum number of bytes for integer value
+MAX_INTEGER_SIZE = 11    # value in -9999999999..99999999999
+
+TAGS = {
+    # Means start of a byte string...
+    '1': "String",
+    '2': "String",
+    '3': "String",
+    '4': "String",
+    '5': "String",
+    '6': "String",
+    '7': "String",
+    '8': "String",
+    '9': "String",
+    'd': "Dictionary",
+    'i': "Integer",
+    'l': "List",
+}
 
 class Entry(FieldSet):
-    TAGS = {
-        # Means start of a byte string...
-        '1': "String",
-        '2': "String",
-        '3': "String",
-        '4': "String",
-        '5': "String",
-        '6': "String",
-        '7': "String",
-        '8': "String",
-        '9': "String",
-        'd': "Dictionary",
-        'i': "Integer",
-        'l': "List"
-    }
+    def parseString(self):
+        addr = self.absolute_address
+#        self._name = "string[]"
+        # <string length encoded in base ten ASCII>:<string data>
+        len = self.stream.searchBytesLength(':', False, addr, addr+(MAX_STRING_LENGTH+1)*8)
+        if len is None:
+            raise ParserError("Torrent: unable to find string separator (':')")
+        val = String(self, "length", len, "String length")
+        yield val
+        try:
+            len = int(val.value)
+        except ValueError:
+            len = -1
+        if len < 0:
+            raise ParserError("Invalid string length (%s)" % makePrintable(val.value, "ASCII", to_unicode=True))
+        yield String(self, "separator", 1, "String length/value separator")
+        if not len:
+            self.info("Empty string: len=%i" % len)
+            return
+        if len<512: # Probably raw data
+            val = String(self, "value", len, "String value")
+            yield val
+            self.info("String '%s': len=%i" % (val.value, len))
+#            self._name = val.value.replace(" ", "_").replace("/", "_")
+        else:
+            yield RawBytes(self, "value", len, "Raw data")
+            self.info("Raw data: len=%i" % len)
+#            self._name = "raw_data"
+
+    def parseDictionary(self):
+        self._name = "dictionary[]"
+        # d<bencoded string><bencoded element>e
+        yield String(self, "start", 1, "Dictionary start delimiter (d)", charset="ASCII")
+        while self.stream.readBytes(self.absolute_address+self.current_size, 1) != "e":
+            yield DictionaryItem(self, "item[]")
+        yield String(self, "end", 1, "Dictionary end delimiter")
+
+    def parseInteger(self):
+        # i<integer encoded in base ten ASCII>e
+        yield String(self, "start", 1, "Integer start delimiter (i)", charset="ASCII")
+        addr = self.absolute_address+self.current_size
+        len = self.stream.searchBytesLength('e', False, addr, addr+(MAX_INTEGER_SIZE+1)*8)
+        if len is None:
+            raise ParserError("Torrent: Unable to find integer end delimiter (e)!")
+        if not len:
+            raise ParserError("Torrent: error, empty integer!")
+        val = String(self, "value", len, "Integer value", charset="ASCII")
+        yield val
+        self.info("%i" % int(val.value))
+        yield String(self, "end", 1, "Integer end delimiter")
+
+    def parseList(self):
+        # l<bencoded values>e
+        self._name = "list[]"
+        yield String(self, "start", 1, "List start delimiter")
+        while self.stream.readBytes(self.absolute_address+self.current_size, 1) != "e":
+            yield Entry(self, "element[]")
+        yield String(self, "end", 1, "List end delimiter")
 
     def createFields(self):
-        type = self.stream.readBytes(self.absolute_address+self.current_size, 1)
-        name = None
-        if type in self.TAGS:
-            name = self.TAGS[type]
-
-        if name:
-            if name == "String":
-                # <string length encoded in base ten ASCII>:<string data>
-                len = self.stream.searchBytesLength(':', False, self.absolute_address+self.current_size)
-                if len is not None:
-                    val = String(self, "length", len, "String length")
-                    yield val
-                    len = int(val.value)
-                    yield String(self, "separator", 1, "String length/value separator")
-                    if len>0:
-                        if len<512: # Probably raw data
-                            val = String(self, "value", len, "String value")
-                            yield val
-                            info("String '%s': len=%i" % (val.value, len))
-                            self._name = val.value.replace(" ", "_").replace("/", "_")
-                        else:
-                            yield RawBytes(self, "value", len, "Raw data")
-                            info("Raw data: len=%i" % len)
-                            self._name = "raw_data"
-                    else:
-                        info("No string: len=%i" % len)
-                        self._name = "string[]"
-                else: self._name = "string[]"
-            elif name == "Dictionary":
-                # d<bencoded string><bencoded element>e
-                yield String(self, "start", 1, "Dictionary start delimiter")
-                while self.stream.readBytes(self.absolute_address+self.current_size, 1) != "e":
-                    #iaddr = self.absolute_address + self.current_size
-                    input = Entry(self, "in[]")
-                    yield input
-                    #oaddr = self.absolute_address + self.current_size
-                    output = Entry(self, "out[]")
-                    yield output
-
-                    # Postprocess names
-                    if input._name == "creation_date":
-                        timestamp = doTimestampUNIX(int(output._name))
-                        output._name = humanDatetime(timestamp).replace(" ", "_")
-                    #if input._name == "length":
-                    #    output._name = humanFilesize(output._name)
-                    #info("%s => %s" % (input._name, output._name))
-                yield String(self, "end", 1, "Dictionary end delimiter")
-                self._name = "dictionary[]"
-            elif name == "Integer":
-                # i<integer encoded in base ten ASCII>e
-                yield String(self, "start", 1, "Integer start delimiter")
-                len = self.stream.searchBytesLength('e', False, self.absolute_address+self.current_size)
-                if len is not None:
-                    if len>0:
-                        val = String(self, "value", len, "Integer value")
-                        yield val
-                        info("%i" % int(val.value))
-                        self._name = val.value
-                    yield String(self, "end", 1, "Integer end delimiter")
-            elif name == "List":
-                # l<bencoded values>e
-                yield String(self, "start", 1, "List start delimiter")
-                while self.stream.readBytes(self.absolute_address+self.current_size, 1) != "e":
-                    yield Entry(self, "element[]")
-                yield String(self, "end", 1, "List end delimiter")
-                self._name = "list[]"
-            else:
-                yield String(self, "unknown[]", "Unknown element %s" % type)
+        type = self.stream.readBytes(self.absolute_address, 1)
+        name = TAGS.get(type, None)
+        if name == "String":
+            for field in self.parseString():
+                yield field
+        elif name == "Dictionary":
+            for field in self.parseDictionary():
+                yield field
+        elif name == "Integer":
+            for field in self.parseInteger():
+                yield field
+        elif name == "List":
+            for field in self.parseList():
+                yield field
         else:
-            info("Entry of type '%s' not handled" % type)
-            yield String(self, "unknown[]", "Unknown element %s" % type)
+            raise ParserError("Torrent: Entry of type %r not handled" % type)
+
+class DictionaryItem(Entry):
+    def createDescription(self):
+        #return "%s=%s" % (self["key"].value, self["value"].display)
+        key = self[0]
+        if "value" in key:
+            key = key["value"].value
+        else:
+            key = key.path
+        value = self[1]
+        if "value" in value:
+            return "%s=%s" % (key, value["value"].value)
+        else:
+            return key
+
+    def createFields(self):
+        input = Entry(self, "key")
+        yield input
+        output = Entry(self, "value")
+        yield output
+
+        # Postprocess names
+        if input._name == "creation_date":
+            timestamp = doTimestampUNIX(int(output._name))
+            output._name = humanDatetime(timestamp).replace(" ", "_")
+        #if input._name == "length":
+        #    output._name = humanFilesize(output._name)
+        #self.info("%s => %s" % (input._name, output._name))
 
 class TorrentFile(Parser):
     endian = LITTLE_ENDIAN
@@ -117,17 +150,17 @@ class TorrentFile(Parser):
         "id": "torrent",
         "category": "misc",
         "file_ext": ("torrent",),
-        "mime": ["application/i-do-not-know"],
         "min_size": 50*8,
+        "magic": ((MAGIC, 0),),
         "description": "Torrent metainfo file"
     }
 
     def validate(self):
-        info(">>>>>>>>>>>>   THERE   <<<<<<<<<<<")
+        self.info(">>>>>>>>>>>>   THERE   <<<<<<<<<<<")
         if self.stream.readBytes(0, len(self.MAGIC)) != self.MAGIC:
             return "Invalid magic"
         return True
 
     def createFields(self):
-        while not self.eof:
-            yield Entry(self, "block[]")
+        yield Entry(self, "root", size=self.size)
+
