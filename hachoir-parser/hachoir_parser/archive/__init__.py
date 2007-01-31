933a41faa09ab2baa8cd7ab9d71dc78110c907ec
haypo <devnull@localhost>
2007-01-31 14:54:19 +0000

    Add parser for Microsoft Cabinet

--- hachoir-parser/hachoir_parser/archive/__init__.py
+++ hachoir-parser/hachoir_parser/archive/__init__.py
@@ -1,6 +1,7 @@
 from hachoir_parser.archive.ace import AceFile
 from hachoir_parser.archive.ar import ArchiveFile
 from hachoir_parser.archive.bzip2_parser import Bzip2Parser
+from hachoir_parser.archive.cab import CabFile
 from hachoir_parser.archive.gzip_parser import GzipParser
 from hachoir_parser.archive.tar import TarFile
 from hachoir_parser.archive.zip import ZipFile
