ab1760375045c77af7477c368fb05f42fe018b18
haypo <devnull@localhost>
2007-03-05 13:30:35 +0000

    Fix OrganizationallyUniqueIdentifier and write NIC24 class

--- hachoir-parser/hachoir_parser/network/common.py
+++ hachoir-parser/hachoir_parser/network/common.py
@@ -1,7 +1,7 @@
-from hachoir_core.field import FieldSet, Bits, Enum
-from hachoir_core.text_handler import textHandler, hexadecimal
+from hachoir_core.field import FieldSet, Bits
 from hachoir_core.bits import str2hex
 from hachoir_parser.network.ouid import REGISTERED_OUID
+from hachoir_core.endian import BIG_ENDIAN
 
 class OrganizationallyUniqueIdentifier(Bits):
     """
@@ -12,31 +12,49 @@ class OrganizationallyUniqueIdentifier(Bits):
     def __init__(self, parent, name, description=None):
         Bits.__init__(self, parent, name, 24, description=None)
 
-    def createValue(self):
-        value = Bits.createValue(self)
-        a = value & 0xFF
-        b = (value >> 8) & 0xFF
-        c = value >> 16
-        return "%02X-%02X-%02X" % (a, b, c)
-
     def createDisplay(self, human=True):
         if human:
-            key = Bits.createValue(self)
+            key = self.value
             if key in REGISTERED_OUID:
                 return REGISTERED_OUID[key]
             else:
-                return self.value
+                return self.raw_display
         else:
-            return self.value
+            return self.raw_display
+
+    def createRawDisplay(self):
+        value = self.value
+        a = value >> 16
+        b = (value >> 8) & 0xFF
+        c = value & 0xFF
+        return "%02X-%02X-%02X" % (a, b, c)
+
+class NIC24(Bits):
+    static_size = 24
+
+    def __init__(self, parent, name, description=None):
+        Bits.__init__(self, parent, name, 24, description=None)
+
+    def createDisplay(self):
+        value = self.value
+        a = value >> 16
+        b = (value >> 8) & 0xFF
+        c = value & 0xFF
+        return "%02x:%02x:%02x" % (a, b, c)
+
+    def createRawDisplay(self):
+        return "0x%06X" % self.value
 
 class MAC48_Address(FieldSet):
     """
     IEEE 802 48-bit MAC address
     """
     static_size = 48
+    endian = BIG_ENDIAN
+
     def createFields(self):
         yield OrganizationallyUniqueIdentifier(self, "organization")
-        yield textHandler(Bits(self, "nic", 24), hexadecimal)
+        yield NIC24(self, "nic")
 
     def hasValue(self):
         return True
