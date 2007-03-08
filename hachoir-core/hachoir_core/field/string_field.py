90757564c7ad4dc1a8d356aa5ddea3adfcb8f7ce
haypo <devnull@localhost>
2007-03-08 14:46:02 +0000

    Add CP037 charset (EBCDIC 037) for Nicolas DUPEUX

--- hachoir-core/hachoir_core/field/string_field.py
+++ hachoir-core/hachoir_core/field/string_field.py
@@ -33,7 +33,7 @@ class GenericString(Bytes):
 
     # 8-bit charsets
     CHARSET_8BIT = set((
-        "ASCII", "MacRoman",
+        "ASCII", "MacRoman", "CP037",
         "ISO-8859-1",  "ISO-8859-2",  "ISO-8859-3",  "ISO-8859-4",
         "ISO-8859-5",  "ISO-8859-6",  "ISO-8859-7",  "ISO-8859-8",
         "ISO-8859-9",  "ISO-8859-10", "ISO-8859-11",
