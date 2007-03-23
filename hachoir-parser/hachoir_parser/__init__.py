e3766f8154e48907f495172964cadfa6e65cf718
haypo <devnull@localhost>
2007-03-23 14:30:28 +0000

    Fix hachoir_parser/__init__.py: remove office

--- hachoir-parser/hachoir_parser/__init__.py
+++ hachoir-parser/hachoir_parser/__init__.py
@@ -3,5 +3,5 @@ from hachoir_parser.parser import ValidateError, HachoirParser, Parser
 from hachoir_parser.parser_list import ParserList, HachoirParserList
 from hachoir_parser.guess import (QueryParser, guessParser, createParser)
 from hachoir_parser import (archive, audio, container,
-    file_system, image, game, misc, network, office, program, video)
+    file_system, image, game, misc, network, program, video)
 
