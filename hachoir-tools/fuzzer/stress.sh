3c815a226e7a728a7dec1632bc126a382c40df70
haypo <devnull@localhost>
2007-04-04 13:32:14 +0000

    fuzzer: ignore 'error' directory in svn, and stress.sh also catch errors

--- hachoir-tools/fuzzer/stress.sh
+++ hachoir-tools/fuzzer/stress.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-TEST_FILES=/home/haypo/mytestcase/ExifTool/
-TEST_FILES=~/waves
-TEST_FILES=~/mytestcase2/Metadata
-TEST_FILES=~/mytestcase2
 TEST_FILES=~/testcase
 GOTCHA=$PWD/error
 MANGLE=$PWD/mangle
-MAX_BLOCK=1000
-PROG="hachoir-metadata --quiet"
 MAX_BLOCK=5000
 PROG="hachoir-grep --all --quiet"
+MAX_BLOCK=5000
+PROG="hachoir-metadata --quiet"
 
 if [ ! -e $MANGLE ]; then
     echo "compile mangle..."
@@ -39,23 +35,21 @@ FILE="/tmp/stres-hachoir"
 trap 'rm -f "$FILE" ; exit 0' INT
 
 NBERROR=0
-while true
-do
-	while true
-	do
-		#INFILE=`(find $TEST_FILES -maxdepth 1 -type f) | perl -ne'rand($.)<=1&&($r=$_);END{print$r}'`
-		INFILE=`find $HOME/mytestcase -type f -size -50000000 -size +1 | perl -ne'rand($.)<=1&&($r=$_);END{print$r}'`
+while true; do
+    while true; do
+        #INFILE=`(find $TEST_FILES -maxdepth 1 -type f) | perl -ne'rand($.)<=1&&($r=$_);END{print$r}'`
+        INFILE=`find $HOME/mytestcase -type f -size -50000000 -size +1 | perl -ne'rand($.)<=1&&($r=$_);END{print$r}'`
                 echo "total: $NBERROR error -- test file: "$(basename "$INFILE")
-		dd if="$INFILE" of="$FILE" count=$MAX_BLOCK 2>/dev/null && \
-		$MANGLE "$FILE" $(wc -c "$FILE") && \
-		$PROG "$FILE" 2>&1 > /dev/null \
-		| grep -q Traceback && break
-		rm "$FILE"
-	done
-        NBERROR=$(expr $NBERROR + 1)
-	SHA=`sha1sum "$FILE" | awk '{print $1}'`
-        ERRNAME="$SHA-"$(basename "$INFILE")
-	mv "$FILE" "$GOTCHA/$ERRNAME"
-	echo "=> ERROR: $ERRNAME"
+        dd if="$INFILE" of="$FILE" count=$MAX_BLOCK 2>/dev/null && \
+        $MANGLE "$FILE" $(wc -c "$FILE") && \
+        $PROG "$FILE" 2>&1 > /dev/null \
+        | egrep -q '(Traceback|error|Error)' && break
+        rm "$FILE"
+    done
+    NBERROR=$(expr $NBERROR + 1)
+    SHA=`sha1sum "$FILE" | awk '{print $1}'`
+    ERRNAME="$SHA-"$(basename "$INFILE")
+    mv "$FILE" "$GOTCHA/$ERRNAME"
+    echo "=> ERROR: $ERRNAME"
 done
 
