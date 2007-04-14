faa775a2916d20a7de1e14ace992b18cb725fb78
haypo <devnull@localhost>
2007-04-14 03:09:25 +0000

    OggFile.validate() only check first 3 pages

--- hachoir-parser/hachoir_parser/container/ogg.py
+++ hachoir-parser/hachoir_parser/container/ogg.py
@@ -258,8 +258,8 @@ class OggFile(Parser):
         magic = OggPage.MAGIC
         if self.stream.readBytes(0, len(magic)) != magic:
             return "Invalid magic string"
-        # Validate first 5 pages
-        for index in xrange(5):
+        # Validate first 3 pages
+        for index in xrange(3):
             try:
                 page = self[index]
             except MissingField:
