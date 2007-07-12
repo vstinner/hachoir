afd573b4bc37e222907b9b441652476137ec78ee
haypo <devnull@localhost>
2007-07-12 23:49:52 +0000

    Set hachoir-urwid and hachoir-metadata version to 1.0.1

--- hachoir-metadata/hachoir_metadata/version.py
+++ hachoir-metadata/hachoir_metadata/version.py
@@ -1,5 +1,5 @@
 PACKAGE = "hachoir-metadata"
-VERSION = "1.0"
+VERSION = "1.0.1"
 WEBSITE = "http://hachoir.org/wiki/hachoir-metadata"
 LICENCE = "GNU GPL v2"
 
--- hachoir-urwid/hachoir_urwid/version.py
+++ hachoir-urwid/hachoir_urwid/version.py
@@ -1,4 +1,4 @@
-VERSION = "1.0"
+VERSION = "1.0.1"
 PACKAGE = "hachoir-urwid"
 WEBSITE = 'http://hachoir.org/wiki/hachoir-urwid'
 LICENSE = 'GNU GPL v2'
