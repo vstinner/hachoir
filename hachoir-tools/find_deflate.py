6fd04898ca851ece55df347fada99852847a5510
haypo <devnull@localhost>
2007-09-06 11:49:22 +0000

    Add function to guess block size

--- hachoir-tools/find_deflate.py
+++ hachoir-tools/find_deflate.py
@@ -4,30 +4,42 @@ from sys import argv, stderr, exit
 
 MIN_SIZE = 2
 
-def can_deflate(compressed_data):
+def canDeflate(compressed_data):
     try:
         data = decompress(compressed_data)
         return True
     except zlib_error:
         return False
 
-
-def find_deflate(data):
+def findDeflateBlocks(data):
     for index in xrange(len(data)-MIN_SIZE):
-        if can_deflate(data[index:]):
+        if canDeflate(data[index:]):
             yield index
+
+def guessDeflateSize(data, offset):
+    size = len(data)-offset
+    while size:
+        if canDeflate(data[offset:offset+size]):
+            yield size
+        size -= 1
 
 def main():
     if len(argv) != 2:
         print >>stderr, "usage: %s filename" % argv[0]
         exit(1)
-    found = False
     data = open(argv[1], 'rb').read()
-    for offset in find_deflate(data):
-        found = True
+    offsets = []
+    for offset in findDeflateBlocks(data):
         print "Offset %s" % offset
-    if not found:
-        print "No deflate block found"
+        offsets.append(offset)
+    if offsets:
+        for offset in offsets:
+            for size in guessDeflateSize(data, offset):
+                if size == (len(data) - offset):
+                    size = "%s (until the end)" % size
+                print "Offset %s -- size %s" % (offset, size)
+    else:
+        print >>stderr, "No deflate block found"
     exit(0)
 
 if __name__ == "__main__":
