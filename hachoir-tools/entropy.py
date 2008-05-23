7e471d6ba19e3677ef2c7ae5a767d426577eb217
haypo <devnull@localhost>
2008-05-23 23:26:12 +0000

    Write class to compute entropy

--- hachoir-tools/entropy.py
+++ hachoir-tools/entropy.py
@@ -1,58 +1,73 @@
 #!/usr/bin/python
 from math import log
-from sys import exit, argv, stderr
-from time import time
 
-BUFFER_SIZE = 4096
+class Entropy:
+    def __init__(self):
+        self.frequence = dict( (chr(index), 0) for index in xrange(0, 256) )
+        self.count = 0
 
-def entropyFile(filename):
-    stream = open(filename, 'rb')
-    stream.seek(0,2)
-    filesize = stream.tell()
-    if filesize == 0:
-        print >>stderr, "Empty file"
-        exit(1)
-    print >>stderr, "Filesize: %s bytes" % filesize
-    stream.seek(0,0)
-    return entropyStream(stream, filesize)
+    def readBytes(self, bytes):
+        for byte in bytes:
+            self.frequence[byte] = self.frequence[byte] + 1
+        self.count += len(bytes)
+        return self
 
-def entropyStream(stream, streamsize):
-    if streamsize <= 0:
-        raise ValueError("Empty stream")
-    # Create list of
-    count = {}
-    for i in range(0, 256):
-        count[ chr(i) ] = 0
-    p = []
-    stream.seek(0)
-    n = 0
-    next_msg = time() + 1.0
-    while True:
-        lastpos = stream.tell()
-        raw = stream.read(BUFFER_SIZE)
-        if not raw:
-            break
-        if next_msg <= time():
-            percent = lastpos * 100.0 / streamsize
-            print >>stderr, "Progress: %.1f%%" % percent
-            next_msg = time() + 1.0
-        n += len(raw)
-        for i in raw:
-            count[i] = count[i] + 1
-    h = 0
-    for i in range(0, 256):
-        i = chr(i)
-        if count[i] != 0:
-            p_i = float(count[i]) / streamsize
+    def compute(self):
+        h = 0
+        for index in xrange(0, 256):
+            index = chr(index)
+            if not self.frequence[index]:
+                continue
+            p_i = float(self.frequence[index]) / self.count
             h -= p_i * log(p_i, 2)
-    return h
+        return h
+
+from time import time
+from sys import stderr
+
+class EntropyFile(Entropy):
+    def __init__(self):
+        Entropy.__init__(self)
+        self.progress_time = 1.0
+        self.buffer_size = 4096
+
+    def displayProgress(self, percent):
+        print >>stderr, "Progress: %.1f%%" % percent
+
+    def readStream(self, stream, streamsize=None):
+        # Read stream size
+        if streamsize is None:
+            stream.seek(0, 2)
+            streamsize = stream.tell()
+        if streamsize <= 0:
+            raise ValueError("Empty stream")
+
+        # Read stream content
+        stream.seek(0,0)
+        next_msg = time() + self.progress_time
+        while True:
+            if next_msg <= time():
+                self.displayProgress(stream.tell() * 100.0 / streamsize)
+                next_msg = time() + self.progress_time
+            raw = stream.read(self.buffer_size)
+            if not raw:
+                break
+            self.readBytes(raw)
+        return self
+
+    def readFile(self, filename):
+        stream = open(filename, 'rb')
+        self.readStream(stream)
+        return self
 
 def main():
+    from sys import argv, exit
     if len(argv) != 2:
         print >>stderr, "usage: %s filename" % argv[0]
         exit(1)
-    value = entropyFile(argv[1])
-    print "Entropy: %.4f bit/byte" % value
+    entropy = EntropyFile()
+    entropy.readFile(argv[1])
+    print "Entropy: %.4f bit/byte" % entropy.compute()
     exit(0)
 
 if __name__ == "__main__":
