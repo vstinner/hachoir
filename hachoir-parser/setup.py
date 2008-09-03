7c5ca8ace7fb11a33cc9b63f1c48f66b161b079a
haypo <devnull@localhost>
2008-09-03 10:08:02 +0000

    hachoir-parser: set version to 1.2, update ChangeLog

--- hachoir-parser/setup.py
+++ hachoir-parser/setup.py
@@ -43,7 +43,7 @@ def main():
         "package_dir": PACKAGES,
     }
     if use_setuptools:
-        install_options["install_requires"] = "hachoir-core>=1.1"
+        install_options["install_requires"] = "hachoir-core>=1.2"
         install_options["zip_safe"] = True
     setup(**install_options)
 
