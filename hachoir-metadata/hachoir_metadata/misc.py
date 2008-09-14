8bbf314aeee18b7ef12b6106a13a3a5a5acdf516
haypo <devnull@localhost>
2008-09-14 12:33:44 +0000

    OLE2 metadata: extract *all* metadata (but ignore some useless ones like code page)

--- hachoir-metadata/hachoir_metadata/misc.py
+++ hachoir-metadata/hachoir_metadata/misc.py
@@ -5,6 +5,7 @@ from hachoir_parser.misc import TorrentFile, TrueTypeFontFile, OLE2_File, PcfFil
 from hachoir_core.field import isString
 from hachoir_core.error import warning
 from hachoir_parser import guessParser
+from hachoir_metadata.setter import normalizeString
 
 class TorrentMetadata(RootMetadata):
     KEY_TO_ATTR = {
@@ -83,18 +84,26 @@ class TTF_Metadata(RootMetadata):
 
 class OLE2_Metadata(RootMetadata):
     SUMMARY_ID_TO_ATTR = {
-         2: ("title", False),
-         4: ("author", False),
-         6: ("comment", False),
-         8: ("author", False),
-         9: ("version", True), # Revision number
-        12: ("creation_date", False),
-        13: ("last_modification", False),
-        14: ("nb_page", False),
-        15: ("comment", True), # Nb. words
-        16: ("comment", True), # Nb. characters
-        18: ("producer", False),
+         2: "title",     # Title
+         3: "title",     # Subject
+         4: "author",
+         6: "comment",
+         8: "author",    # Last saved by
+        12: "creation_date",
+        13: "last_modification",
+        14: "nb_page",
+        18: "producer",
     }
+    IGNORE_SUMMARY = set((
+        1, # Code page
+    ))
+
+    DOC_SUMMARY_ID_TO_ATTR = {
+        14: "author",    # Manager
+    }
+    IGNORE_DOC_SUMMARY = set((
+        1, # Code page
+    ))
 
     def extract(self, ole2):
         self._extract(ole2)
@@ -105,10 +114,12 @@ class OLE2_Metadata(RootMetadata):
             fieldset._feedAll()
             if "root[0]" in fieldset:
                 self.useRoot(fieldset["root[0]"])
-        if "summary[0]" in fieldset:
-            # _feedAll() is needed to make sure that we get all summary[*] fragments
-            fieldset._feedAll()
-            self.useSummary(fieldset["summary[0]"], main_document)
+        summary = self.getField(fieldset, main_document, "summary[0]")
+        if summary:
+            self.useSummary(summary, False)
+        doc_summary = self.getField(fieldset, main_document, "doc_summary[0]")
+        if doc_summary:
+            self.useSummary(doc_summary, True)
 
     @fault_tolerant
     def useRoot(self, root):
@@ -119,36 +130,63 @@ class OLE2_Metadata(RootMetadata):
             return
         self._extract(ministream, main_document=False)
 
-    def useSummary(self, summary, main_document):
+    def getField(self, fieldset, main_document, name):
+        if name not in fieldset:
+            return None
+        # _feedAll() is needed to make sure that we get all fragments
+        # eg. summary[0], summary[1], ..., summary[n]
+        fieldset._feedAll()
+        field = fieldset[name]
         if main_document:
-            stream = summary.getSubIStream()
-            summary = guessParser(stream)
-            if not summary:
-                warning("Unable to create the OLE2 summary parser!")
-                return
+            stream = field.getSubIStream()
+            field = guessParser(stream)
+            if not field:
+                warning("Unable to create the OLE2 parser for %s!" % name)
+                return None
+        return field
 
+    def useSummary(self, summary, is_doc_summary):
         if "os" in summary:
             self.os = summary["os"].display
         if "section[0]" not in summary:
             return
         summary = summary["section[0]"]
         for property in summary.array("property_index"):
-            self.useProperty(summary, property)
+            self.useProperty(summary, property, is_doc_summary)
 
     @fault_tolerant
-    def useProperty(self, summary, property):
+    def useProperty(self, summary, property, is_doc_summary):
         field = summary.getFieldByAddress(property["offset"].value*8)
         if not field:
             return
         if not field.hasValue():
             return
+
+        # Get value
         value = field.value
-        try:
-            key, use_prefix = self.SUMMARY_ID_TO_ATTR[property["id"].value]
-        except LookupError:
-#                warning("Skip %s[%s]=%s" % (
-#                    property["id"].display, property["id"].value, value))
+        if isinstance(value, (str, unicode)):
+            value = normalizeString(value)
+            if not value:
+                return
+
+        # Get property identifier
+        prop_id = property["id"].value
+        if is_doc_summary:
+            id_to_attr = self.DOC_SUMMARY_ID_TO_ATTR
+            ignore = self.IGNORE_DOC_SUMMARY
+        else:
+            id_to_attr = self.SUMMARY_ID_TO_ATTR
+            ignore = self.IGNORE_SUMMARY
+        if prop_id in ignore:
             return
+
+        # Get Hachoir metadata key
+        try:
+            key = id_to_attr[prop_id]
+            use_prefix = False
+        except LookupError:
+            key = "comment"
+            use_prefix = True
         if use_prefix:
             value = "%s: %s" % (property["id"].display, value)
         setattr(self, key, value)
