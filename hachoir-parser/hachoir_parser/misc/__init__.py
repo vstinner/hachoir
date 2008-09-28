a7b378e2a20955917ed1c978aaea87967261f24c
haypo <devnull@localhost>
2008-09-28 16:20:39 +0000

    Create Apple/NeXT Binary Property List (BPLIST) parser

--- hachoir-parser/hachoir_parser/misc/__init__.py
+++ hachoir-parser/hachoir_parser/misc/__init__.py
@@ -10,4 +10,5 @@ from hachoir_parser.misc.pdf import PDFDocument
 from hachoir_parser.misc.pifv import PIFVFile
 from hachoir_parser.misc.hlp import HlpFile
 from hachoir_parser.misc.gnome_keyring import GnomeKeyring
+from hachoir_parser.misc.bplist import BPList
 
