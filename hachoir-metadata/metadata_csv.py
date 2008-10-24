bf55513785b3d14e871219424eba733f3aa4fb97
haypo <devnull@localhost>
2008-10-24 10:32:09 +0000

    metadata_csv.py:  * Create a class (Extractor)  * Command line has a second mandatory argument: fields  * Write output to a .csv file

--- hachoir-metadata/metadata_csv.py
+++ hachoir-metadata/metadata_csv.py
@@ -12,64 +12,78 @@ from fnmatch import fnmatch
 from datetime import datetime
 import codecs
 
+
 OUTPUT_FILENAME = "metadata.csv"
 
-def processFile(counter, filename, charset, output):
-    filename, realname = unicodeFilename(filename), filename
-    print u"[%s] Process file %s..." % (counter, filename)
-    parser = createParser(filename, realname)
-    if not parser:
-        print >>stderr, "Unable to parse file"
-        return False
-    try:
-        metadata = extractMetadata(parser)
-    except HachoirError, err:
-        print >>stderr, "Metadata extraction error: %s" % unicode(err)
-        return False
-    if not metadata:
-        print >>stderr, "Unable to extract metadata"
-        return False
+class Extractor:
+    def __init__(self, directory, fields):
+        self.directory = directory
+        self.fields = fields
+        self.charset = "UTF-8"
+        self.total = 0
+        self.invalid = 0
 
-    title = metadata.get('title', '')
-    creation = metadata.get('creation_date', datetime(1970, 1, 1))
-    author = metadata.get('author', '')
+    def main(self):
+        output = codecs.open(OUTPUT_FILENAME, "w", self.charset)
+        for filename in self.findFiles(self.directory, '*.doc'):
+            self.total += 1
+            line = self.processFile(filename)
+            if line:
+                print >>output, line
+            else:
+                self.invalid += 1
+        output.close()
+        self.summary()
 
-    filename = makePrintable(filename, charset, to_unicode=True)
-    title = makePrintable(title, charset, to_unicode=True)
-    print >>output, u"%s; %s; %s; %s" % (filename, title, creation, author)
-    return True
+    def summary(self):
+        print >>stderr
+        print >>stderr, "Valid files: %s" % (self.total - self.invalid)
+        print >>stderr, "Invalid files: %s" % self.invalid
+        print >>stderr, "Total files: %s" % self.total
+        print >>stderr
+        print >>stderr, "Result written into %s" % OUTPUT_FILENAME
 
-def findFiles(directory, pattern):
-    for dirpath, dirnames, filenames in walk(directory):
-        for filename in filenames:
-            if not fnmatch(filename.lower(), pattern):
-                continue
-            yield path_join(dirpath, filename)
+    def findFiles(self, directory, pattern):
+        for dirpath, dirnames, filenames in walk(directory):
+            for filename in filenames:
+                if not fnmatch(filename.lower(), pattern):
+                    continue
+                yield path_join(dirpath, filename)
+
+    def processFile(self, filename):
+        filename, realname = unicodeFilename(filename), filename
+        print u"[%s] Process file %s..." % (self.total, filename)
+        parser = createParser(filename, realname)
+        if not parser:
+            print >>stderr, "Unable to parse file"
+            return None
+        try:
+            metadata = extractMetadata(parser)
+        except HachoirError, err:
+            print >>stderr, "Metadata extraction error: %s" % unicode(err)
+            return None
+        if not metadata:
+            print >>stderr, "Unable to extract metadata"
+            return None
+
+        filename = makePrintable(filename, self.charset, to_unicode=True)
+        line = [filename]
+        for field in self.fields:
+            value = metadata.getText(field, u'')
+            value = makePrintable(value, self.charset, to_unicode=True)
+            line.append(value)
+        return '; '.join(line)
 
 def main():
     initLocale()
-    if len(argv) != 2:
-        print >>stderr, "usage: %s directory" % argv[0]
+    if len(argv) != 3:
+        print >>stderr, "usage: %s directory fields" % argv[0]
+        print >>stderr
+        print >>stderr, "eg. %s . title,creation_date" % argv[0]
         exit(1)
     directory = argv[1]
-
-    charset = "UTF-8"
-    output = codecs.open(OUTPUT_FILENAME, "w", charset)
-    total = 0
-    invalid = 0
-    for filename in findFiles(directory, '*.doc'):
-        total += 1
-        ok = processFile(total, filename, charset, output)
-        if not ok:
-            invalid += 1
-    output.close()
-
-    print
-    print "Valid files: %s" % (total - invalid)
-    print "Invalid files: %s" % invalid
-    print "Total files: %s" % total
-    print
-    print "Result written into %s" % OUTPUT_FILENAME
+    fields = [field.strip() for field in argv[2].split(",")]
+    Extractor(directory, fields).main()
 
 if __name__ == "__main__":
     main()
