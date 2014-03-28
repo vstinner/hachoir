295ecfbc060760ecd2f4f94a1ea86aa9b70e1de8
Victor Stinner <victor.stinner@gmail.com>
2014-03-28 16:33:19 +0100

    Fix Language: replace __cmp__

--- hachoir/core/language.py
+++ hachoir/core/language.py
@@ -1,5 +1,7 @@
+import functools
 from hachoir.core.iso639 import ISO639_2
 
+@functools.total_ordering
 class Language:
     def __init__(self, code):
         code = str(code)
@@ -7,17 +9,18 @@ class Language:
             raise ValueError("Invalid language code: %r" % code)
         self.code = code
 
-    def __cmp__(self, other):
+    def __eq__(self, other):
         if other.__class__ != Language:
-            return 1
-        return cmp(self.code, other.code)
+            return NotImplemented
+        return self.code == other.code
 
-    def __unicode__(self):
-       return ISO639_2[self.code]
+    def __lt__(self, other):
+        if other.__class__ != Language:
+            return NotImplemented
+        return self.code < other.code
 
     def __str__(self):
-       return self.__unicode__()
+       return ISO639_2[self.code]
 
     def __repr__(self):
         return "<Language '%s', code=%r>" % (str(self), self.code)
-
