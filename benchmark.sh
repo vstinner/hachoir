2c954275fdab206d35d1c94a9548e3ae08e7497f
Victor Stinner <victor.stinner@gmail.com>
2014-05-26 14:05:18 +0200

    fix benchmark.sh: adapt new paths

--- benchmark.sh
+++ benchmark.sh
@@ -4,9 +4,9 @@ if [ "x$PYTHON" = "x" ]; then
    PYTHON=`which python3`
 fi
 PYTHON=$PYTHON
-ROOT=$(cd `dirname $0`; pwd)
-TESTCASE=testcase
-export PYTHONPATH=$ROOT/hachoir-core:$ROOT/hachoir-parser:$ROOT/hachoir-metadata:$PYTHONPATH
+SRC=$(cd `dirname $0`; pwd)
+TESTCASE=tests/files/
+export PYTHONPATH=$SRC/hachoir-core:$SRC/hachoir-parser:$SRC/hachoir-metadata:$PYTHONPATH
 
 function prepare_benchmark
 {
@@ -20,10 +20,10 @@ PYTHON_VERSION=`$PYTHON -c 'from sys import version; print(version.split("\n")[0
 echo "Benchmark Hachoir version $HACHOIR_VERSION on Python $PYTHON_VERSION"
 
 prepare_benchmark "hachoir-grep: yellowcase"
-$PYTHON -OO $ROOT/hachoir-tools/hachoir-grep --bench --all $TESTCASE/yellowdude.3ds
+$PYTHON -OO $SRC/hachoir-grep --bench --all $TESTCASE/yellowdude.3ds
 
 prepare_benchmark "hachoir-metadata: set A (mp3, wav, png, au, mkv)"
-(cd $TESTCASE; $PYTHON -OO $ROOT/hachoir-metadata \
+(cd $TESTCASE; $PYTHON -OO $SRC/hachoir-metadata \
     --bench \
     sheep_on_drugs.mp3 kde_click.wav logo-kubuntu.png \
     audio_8khz_8bit_ulaw_4s39.au flashmob.mkv 10min.mkv)
