ce0d835415ebebf770587e984508e068dcbd09f2
Robert Xiao <brx@cs.cmu.edu>
2017-01-10 02:49:04 -0500

    wx: Work around wxPython bug

--- hachoir/wx/resource/resource.py
+++ hachoir/wx/resource/resource.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import os
-from wx.xrc import XmlResource, XRCCTRL
+from wx.xrc import XmlResource, XRCID
 
 def get_resource():
     filename = os.path.join(os.getcwd(), os.path.dirname(__file__), 'hachoir_wx.xrc')
@@ -11,7 +11,11 @@ def get_frame(name):
     return get_resource().LoadFrame(None, name)
 
 def get_child_control(parent, child):
-    return XRCCTRL(parent, child)
+    # We do this instead of XRCCTRL to work around a bug in wxPython 3.0.3.
+    # FindWindowById, FindWindowByName and XRCCTRL all seem to return the
+    # first-created "child" instead of the proper one; only FindWindow behaves
+    # as expected.
+    return parent.FindWindow(XRCID(child))
 
 def get_menu_bar(name):
     return get_resource().LoadMenuBar(name)
