4822dc3d384e978ddbbb2a7df2d459bfa9a6bc62
Victor Stinner <vstinner@redhat.com>
2017-02-17 21:38:33 +0100

    Move script code into files in hachoir/

--- tools/flake8.sh
+++ tools/flake8.sh
@@ -3,8 +3,4 @@ set -e -x
 cd $(dirname "$0")/..
 # use /bin/sh to support "*.py"
 # FIXME: add hachoir-wx (currrently broken)
-flake8 \
-    hachoir/ tests/ \
-    runtests.py setup.py \
-    hachoir-{grep,metadata,metadata-csv,metadata-gtk,metadata-qt,strip,subfile,urwid} \
-    doc/examples/*.py
+flake8 hachoir/ tests/ runtests.py setup.py doc/examples/*.py
