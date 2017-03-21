23ba991129f0270b78d5d6725d382e0b6cbe1e2f
nosmokingbandit <watchernzb@gmail.com>
2017-03-21 05:26:46 -0400

    Add method to export metadata in dictionary (#16)
    * Add method to export metadata in dictionary
    
    Used almost identically to exportPlaintext. Exports metadata in an easy to access dictionary.
    
    * Fix comments and whitespace
    
    * Add unittest for exportDictionary
    
    * Replace missing endquote
    
    Stupid fat fingers
    
    * Switch to context manager

--- hachoir/metadata/metadata.py
+++ hachoir/metadata/metadata.py
@@ -179,6 +179,45 @@ class Metadata(Logger):
         else:
             return None
 
+    def exportDictionary(self, priority=None, human=True, title=None):
+        r"""
+        Convert metadata to python Dictionary and skip datas
+        with priority lower than specified priority.
+
+        Default priority is Metadata.MAX_PRIORITY. If human flag is True, data
+        key are translated to better human name (eg. "bit_rate" becomes
+        "Bit rate") which may be translated using gettext.
+
+        If priority is too small, metadata are empty and so None is returned.
+
+        """
+        if priority is not None:
+            priority = max(priority, MIN_PRIORITY)
+            priority = min(priority, MAX_PRIORITY)
+        else:
+            priority = MAX_PRIORITY
+        if not title:
+            title = self.header
+        text = {}
+        text[title] = {}
+        for data in sorted(self):
+            if priority < data.priority:
+                break
+            if not data.values:
+                continue
+            if human:
+                field = data.description
+            else:
+                field = data.key
+            text[title][field] = {}
+            for item in data.values:
+                if human:
+                    value = item.text
+                else:
+                    value = makeUnicode(item.value)
+                text[title][field] = value
+        return text
+
     def __bool__(self):
         return any(item for item in self.__data.values())
 
@@ -251,6 +290,22 @@ class MultipleMetadata(RootMetadata):
             return text
         else:
             return None
+
+    def exportDictionary(self, priority=None, human=True):
+        common = Metadata.exportDictionary(self, priority, human)
+        if common:
+            text = common
+        else:
+            text = {}
+        for key, metadata in self.__groups.items():
+            if not human:
+                title = key
+            else:
+                title = None
+            value = metadata.exportDictionary(priority, human, title=title)
+            if value:
+                text.update(value)
+        return text
 
 
 def registerExtractor(parser, extractor):
