08065148df1ea03a1695bed19f5c121abae81fd2
espes <espes@pequalsnp.com>
2018-03-20 13:20:23 +0000

    video/mov.py -> container/mp4.py

--- hachoir/parser/container/__init__.py
+++ hachoir/parser/container/__init__.py
@@ -4,3 +4,4 @@ from hachoir.parser.container.ogg import OggFile, OggStream  # noqa
 from hachoir.parser.container.riff import RiffFile  # noqa
 from hachoir.parser.container.swf import SwfFile  # noqa
 from hachoir.parser.container.realmedia import RealMediaFile  # noqa
+from hachoir.parser.container.mp4 import MP4File  # noqa
